a9b42f5a65d73ea5d6c061c50ee8846d15b13b4f
unknown <ok.brede@inbox.lv>
2022-05-09 14:03:56 +0300

    A practikum 18.6 is added

---
+++ title.py
@@ -0,0 +1,9 @@
+
+TOKEN = "5343764665:AAFRCpXCL5u-hYHSgQVAULXwCYjH7Mxslqc"  #"Токен, полученный при регистрации"
+
+keys = {"рубль": "RUB",
+        "доллар": "USD",
+        "евро": "EUR",
+        "злотый": "PLN",
+        "иена": "JPY"}
+
